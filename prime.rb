09db02a043e44d159463a8194e4dc30357833054
Maharshee roy <m.roy@optonline.net>
2018-11-04 21:53:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
-def prime?(num)
-  if num <= 1
+def prime?(int)
+  if int <= 1
      return false
    end
-   for d in 2..(num - 1)
-     if (num % d) == 0
+   for d in 2..(int - 1)
+     if (int % d) == 0
        return false
      end
    end
