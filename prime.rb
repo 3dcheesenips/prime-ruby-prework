135f35bb747c8f3b50ade7c1bd32bcdfb30500d9
Maharshee roy <m.roy@optonline.net>
2018-11-04 21:52:35 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,13 @@
 # Add  code here!
 def prime?(int)
-  if int <= 1 
-    return false 
-  end
-  for i in (2...num)
-    if (int % i) == 0 
+
+   if num <= 1
+     return false
+   end
+   for i in (2...num)
+    if (num % i) == 0
       return false
     end
-  end
-  return true
-end
+   end
+    true
+ end
